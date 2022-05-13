10d597734ef4fb24d99d52c514a5ae500ad51f39
Wieslaw Samushonga <72025253+JxstWieslaw@users.noreply.github.com>
2022-05-13 18:30:15 +0530

    environment variables

---
+++ 26_try-on-gallery/features/23-r3f-oauth2/extras.js
@@ -0,0 +1,3 @@
+MONGO_URI=mongodb+srv://jxstwieslaw:sams1234@cluster1.upu98.mongodb.net/Users?retryWrites=true&w=majority
+
+clientId="191635117542-cfncvg94338rjd3c7b90o8q3rjl2ghhr.apps.googleusercontent.com"
